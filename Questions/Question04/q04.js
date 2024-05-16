81d09ad4ccd9240b9cf121fb80cd7d7a03d0230d
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-05-16 15:33:19 +0500

    Question no 04 Added

--- Questions/Question04/q04.js
+++ Questions/Question04/q04.js
@@ -1,13 +1,57 @@
-function randomValueGenerator() {
-    let randomDiceValue = Math.floor(Math.random() * 6);
-    let diceSide = randomDiceValue + 1;
-    document.write(`<div class="card">
-    <h4>Printing Dice Value</h4>
-    <span> Random Dice Value : ${diceSide} </span>
-    </div>`);
+let formContainer = document.getElementById("form-container");
+let formContent = document.getElementById("form-content");
+let formInfoContainer = document.getElementById("info-container");
+// console.log(formInfoContainer);
+// console.log(form);
+let userName;
+let gender;
+let address;
+let education;
+let profession;
+
+function CitizenInfo(userName, gender, address, education, profession) {
+    this.Name = userName,
+        this.Gender = gender,
+        this.HomeAddress = address,
+        this.Education = education,
+        this.Profession = profession
 }
 
-randomValueGenerator();
-randomValueGenerator();
-randomValueGenerator();
-randomValueGenerator();
+let nameInput = document.getElementById("name-input");
+let addressInput = document.getElementById("address-input");
+let professionInput = document.getElementById("profession-input");
+let genderInput = document.getElementById("gender-input");
+let educationInput = document.getElementById("education-input");
+
+function catchGender(elementGender) {
+    gender = elementGender.value
+}
+
+function catchEducation(elementEducation) {
+    education = elementEducation.value
+}
+
+function submitValues() {
+    userName = nameInput.value;
+    address = addressInput.value;
+    profession = professionInput.value;
+    console.log(userName, gender, address, profession, education);
+
+    formContent.style.display = "none";
+    formInfoContainer.style.display = "flex";
+
+    let formHeading = document.getElementById("form-heading");
+    formHeading.innerText = "Printing User Entered Details in Browser"
+
+    
+    var user1 = new CitizenInfo(userName, gender, address, education, profession);
+    console.log(user1);
+    formInfoContainer.innerHTML =  `
+        <span>FullName :<strong> ${user1.Name} </strong></span>
+        <span>Address :<strong> ${user1.HomeAddress}</strong> </span>
+        <span>Profession :<strong> ${user1.Profession}</strong></span>
+        <span>Gender :<strong> ${user1.Gender}</strong></span>
+        <span>Education :<strong> ${user1.Education}</strong></span>`
+}
+
+
